b279951646af4fb88fb1a2a66966edbc70f6ad58
Roderick Mendoza <rodmendoza07@gmail.com>
2023-02-23 16:27:17 -0800

    create move method and rspec

--- lib/towers_of_hanoi.rb
+++ lib/towers_of_hanoi.rb
@@ -1,13 +1,20 @@
 # class TowersOfHanoi
 class TowersofHanoi
-
+  attr_reader :stacks, :max_height
   def initialize(max_height)
-    @stacks = Stacks.new(max_height)
+    @max_height = max_height
+    @stacks = Array.new(3) { Array.new }
+
+    (1..max_height).each do |i|
+      @stacks[0] <<i
+    end
+
   end
 
-  def move
-    @popped = @stacks.pop
-    @stack
+  def move(start, destination)
+    disk = self.stacks[start].shift
+    self.stacks[destination].unshift(disk)
+    self.stacks
   end
 
   def won?
@@ -15,17 +22,4 @@ class TowersofHanoi
 
   def valid_move?
   end
-end
-
-class Stacks
-
-  attr_accessor :stacks
-  def initialize(max_height)
-    @max_height = max_height
-    max_height.times do
-      @stacks = Array.new(max_height)
-    end
-    end
-  end
-
 end
--- spec/towers_of_hanoi_spec.rb
+++ spec/towers_of_hanoi_spec.rb
@@ -1,12 +1,29 @@
 require 'rspec'
 require 'towers_of_hanoi'
 
-describe TowersOfHanoi do
+describe 'TowersOfHanoi' do
+  subject { TowersofHanoi.new(3) }
 
   describe "#initialize" do
+    it "should create an array of three towers" do
+    expect(subject.stacks.length).to eq(3)
+    end
+
+    it "should create a full stack at the first rod and two empty stacks for the next two" do
+    expect(subject.stacks[0]).to eq((1..subject.max_height).to_a)
+    expect(subject.stacks[1]).to eq([])
+    expect(subject.stacks[2]).to eq([])
+    end
   end
-  
+
   describe "#move" do
+    it "should move a disk from the top of the given stack and place the disk on top of the given stack" do
+      expect(subject.move(0, 2)).to eq([[2,3], [], [1]])
+    end
+
+    it "should only place a disk in a stack if the disk is smaller than the top disk of the given tower" do
+    end
+
   end
 
   describe "#won?" do
